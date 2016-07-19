f9c32e7ebba8faa0f21e6a3a79a2a43c17a9df1b
Yash Verma <verma.yash8@gmail.com>
2016-07-19 23:21:11 +0530

    Changed name to MusicRemote

---
+++ app/src/main/java/commandoengineer/musiccontrol/ControllingActivity.java
@@ -0,0 +1,13 @@
+package commandoengineer.musiccontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControllingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controlling);
+    }
+}
