9b94abbb23121bfd9936377e44fa9a8fce58daa3
Yash Verma <verma.yash8@gmail.com>
2016-07-19 23:21:56 +0530

    Before Pushing

--- app/src/main/java/commandoengineer/musiccontrol/ControllingActivity.java
+++ app/src/main/java/commandoengineer/musiccontrol/ControllingActivity.java
@@ -1,13 +1,133 @@
 package commandoengineer.musiccontrol;
 
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ControllingActivity extends AppCompatActivity {
+    public static final String  TAG = "ControllingActivity";
 
+    Socket s;
+    DataOutputStream dOut ;
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controlling);
+
+        Button vol_up = (Button) findViewById(R.id.vol_hi);
+        Button vol_down = (Button) findViewById(R.id.vol_lo);
+        Button prev = (Button) findViewById(R.id.prev);
+        Button play = (Button) findViewById(R.id.play);
+        Button next = (Button) findViewById(R.id.next);
+
+        vol_up.setOnClickListener(new ClickListener());
+        vol_down.setOnClickListener(new ClickListener());
+        prev.setOnClickListener(new ClickListener());
+        play.setOnClickListener(new ClickListener());
+        next.setOnClickListener(new ClickListener());
+
+        new GetIPTask().execute();
+        new ServerTask().execute();
     }
+
+    public class GetIPTask extends AsyncTask<Void,Void,String>{
+        @Override
+        protected String doInBackground(Void... params) {
+            Log.e(TAG,"Doing");
+            String result = "";
+            try {
+                result = InetAddress.getLocalHost().toString();
+                Log.e(TAG,"Getting result="+result);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            TextView ipDisplay = (TextView) findViewById(R.id.ipDisplay);
+            ipDisplay.setText(s);
+        }
+    }
+
+    public class ClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            int message = 0;
+            switch(v.getId()){
+                case R.id.vol_hi :  message = 9;
+                    break;
+                case R.id.vol_lo : message = 8;
+                    break;
+                case R.id.prev : message = 0;
+                    break;
+                case R.id.play : message = 1;
+                    break;
+                case R.id.next : message = 2;
+                    break;
+            }
+
+            Log.e(TAG,"Button Clicked= "+message);
+            Log.e(TAG,"Writing Msg = "+message);
+            try {
+                dOut.writeInt(message);
+                dOut.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class CustomRunnable implements Runnable{
+        int msg ;
+        public CustomRunnable(int msg){
+            this.msg = msg;
+        }
+        @Override
+        public void run() {
+            try {
+                Log.e(TAG,"Writing Msg = "+msg);
+                dOut.writeInt(msg);
+                dOut.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    public class ServerTask extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected Void doInBackground(Void... params) {
+            int PORT_NO = 6000;
+
+            try {
+                ServerSocket ss = new ServerSocket(PORT_NO);
+                s = ss.accept();
+                dOut = new DataOutputStream(s.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+
 }
--- app/src/main/java/commandoengineer/musiccontrol/MainActivity.java
+++ app/src/main/java/commandoengineer/musiccontrol/MainActivity.java
@@ -1,7 +1,9 @@
 package commandoengineer.musiccontrol;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +15,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +24,31 @@ public class MainActivity extends AppCompatActivity {
         final EditText inputIP = (EditText) findViewById(R.id.inputIP);
         final EditText inputPort = (EditText) findViewById(R.id.inputPort);
         final Button start = (Button) findViewById(R.id.start);
-        final Button stop = (Button) findViewById(R.id.stop);
+        final Button useAsRemoteButton = (Button) findViewById(R.id.useAsRemoteButton);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String recentIP = prefs.getString("recentIP","10.42.0.1");
+        inputIP.setText(recentIP);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String ip = inputIP.getText().toString();
+                SharedPreferences.Editor ed = prefs.edit();
+                ed.putString("recentIP",ip).commit();
+
                 int portNo = Integer.parseInt(inputPort.getText().toString());
                 Intent intent = new Intent(MainActivity.this,ControllerService.class);
                 intent.putExtra(Constants.SERVER_IP_EXTRA,ip);
                 intent.putExtra(Constants.SERVER_PORT,portNo);
                 MainActivity.this.startService(intent);
                 start.setText("Started");
+            }
+        });
+
+        useAsRemoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ControllingActivity.class));
             }
         });
     }
